6c6e0325b3c460636dcee555828b8c123ebe7582
amare <amare2020@gmail.com>
2025-02-07 22:39:36 +0300

    Javascript exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...element) {
+    return array.filter(item => !element.includes(item));
+
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+// console.log(removeFromArray([1, 2, 3, 4, 5, 6], 4, 5));
+// 
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,28 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    if (num1 <= 0 || num2 <= 0 || Math.floor(num1) !== num1 || Math.floor(num2) !== num2) {
+        return "ERROR";
+    }
+
+    else if (num1 <= num2) {
+        for(let i = num1; i <= num2; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+
+
+    else {
+        for(let i = num2; i <= num1; i++) {
+            sum += i;
+        }
+        return sum;
+
+    }
 
 };
+// console.log(4+5+6+7+8+9+10+11);
+// console.log(sumAll(4, 11));
 
 // Do not edit below this line
 module.exports = sumAll;
