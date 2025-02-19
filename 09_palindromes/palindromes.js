3c9e4c6c863ea89077f89facc04591e53fd8943d
amare <amare2020@gmail.com>
2025-02-19 21:37:10 +0300

    Solution for Exercise 09 - Palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    // Normalize the string: remove non-alphanumeric characters and convert to lowercase
+  const cleanedStr = str.replace(/[^a-z0-9]/gi, '').toLowerCase();
+  
+  // Check if the cleaned string is the same forwards and backwards
+  const reversedStr = cleanedStr.split('').reverse().join('');
+  
+  return cleanedStr === reversedStr;
+    
 };
 
 // Do not edit below this line
