0e8e9e54d8b6896d1bc6299106d1b00d6bb36edd
amare <amare2020@gmail.com>
2025-02-19 22:42:32 +0300

    Solution for Exercise 12 - Find the Oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldest = null;
+    for (const person of people) {
+        const age = (person.yearOfDeath || new Date().getFullYear()) - person.yearOfBirth;
+        if (oldest === null || age > oldest.age) {
+            oldest = {
+                name: person.name,
+                age: age
+            };
+        }
+    }
+    return oldest;
 };
 
 // Do not edit below this line
