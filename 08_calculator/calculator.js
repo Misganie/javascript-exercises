5af9313bb022864a4329b3ecca27dcd064d0bcea
amare <amare2020@gmail.com>
2025-02-19 20:59:48 +0300

    Solution for Exercise 08 - Calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(numOne, numTwo) {
+	return numOne + numTwo;
 };
 
-const subtract = function() {
-	
+const subtract = function(numOne, numTwo) {
+	return numOne - numTwo;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  return numbers.reduce((accumulator, current) => {
+    const num = Number(current); // Convert current to a number
+    return isNaN(num) ? accumulator : accumulator + num; // Ignore invalid numbers
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  return numbers.reduce((accumulator, current) => {
+    const num = Number(current); // Convert current to a number
+    return isNaN(num) ? accumulator : accumulator * num; // Ignore invalid numbers
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(numOne, numTwo) {
+	return numOne ** numTwo;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let result = 1;
+  for (let i = n; i >= 1; i--) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
